7cc5e620ca6da75bc87c01588096c9076e057cf0
Angel Legorreta <legosurio@hotmail.com>
2022-09-04 02:03:20 -0500

    avances de la api

---
+++ ProjectSettings/Assets/Code/Jokes.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Jokes", menuName = "NightmareStudios/Jokes")]
+
+public class Jokes : ScriptableObject
+{
+   public string jokes;
+   public string setup;
+   public string delivery;
+}
