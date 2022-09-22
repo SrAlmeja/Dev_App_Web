d29c047551ea2d5a5a9386bea77d00fce69eb34c
Sr Almeja <cdmx1895@amerike.edu.mx>
2022-09-22 18:51:54 -0500

    Avances de hoy

---
+++ ProjectSettings/Assets/AlmejaStudio/Code/UnityClient.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using TMPro;
+using UnityEngine.UI;
+
+public class UnityClient : MonoBehaviour
+{
+    public TMP_Text Text;
+
+    //public Buttom 
+    
+    void Start()
+    {
+        // A correct website page.
+        StartCoroutine(GetRequest("https://localhost:44363/api/todo"));
+        // A non-existing page.
+        //StartCoroutine(GetRequest("https://error.html"));
+    }
+   
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    JSONNode root = JSONNode.Parse(webRequest.downloadHandler.text);
+                    
+                    foreach (var key in root.Keys)
+                    {
+                        Text.text = (pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    }
+                    
+                    break; 
+            }
+
+        }
+    }
+    public void PrubeButton()
+    {
+        StartCoroutine(GetRequest("https://localhost:44363/api/todo"));
+    }
+}
