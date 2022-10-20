9ee6ff7e24eabc14dc210ba5ff2fd1c33490d3df
Inquisidor <cdmx1895@amerike.edu.mx>
2022-10-20 18:49:42 -0500

    my Web

---
+++ MyWeb/MyWeb1/MyWeb1/Controllers/MyFirstController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWeb1.Controllers;
+
+public class MyFirstController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
